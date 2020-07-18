14e24ab8f8f98875135474185c3702f9f5dd9f25
Lucas <ldanezine@gmail.com>
2020-07-17 21:49:33 -0300

    Adicionar arquivos de projeto.

---
+++ ExcClasseAbstrataTwo/Entities/Contribuinte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcClasseAbstrataTwo.Entities
+{
+    abstract class Contribuinte
+    {
+        public string Nome { get; set; }
+        public double RendaAnual { get; set; }
+
+        public abstract double taxa(double renda);
+            
+    }
+}
